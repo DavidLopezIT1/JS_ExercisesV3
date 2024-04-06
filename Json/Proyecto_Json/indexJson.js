471ce71a9c75f7ada9f561e1ac0688afb91ea4b0
DavidLopezIT1 <santyfcw@gmail.com>
2024-04-06 00:18:29 -0500

    Json fetch + Evaluación finalizada

---
+++ Json/Proyecto_Json/indexJson.js
@@ -0,0 +1,37 @@
+// let banco ;
+// let sucursal ;
+// let titular ;
+// let nro_cuenta ;
+// let saldo ;
+// let cbu ;
+// let abierto ;
+
+
+// fetch('resumen.json').then(promesa => promesa.json())
+// .then((salida) => {
+//     if( salida.status === 200){
+//         banco = salida.banco,
+//         sucursal = salida.sucursal,
+//         titular = salida.titular,
+//         nro_cuenta = salida.nro_cuenta,
+//         saldo = salida.saldo,
+//         cbu = salida.cbu,
+//         abierto = salida.abierto
+
+//             let Banco = document.getElementById('Banco');
+//             let Cliente = document.getElementById('Cliente');
+//             let MontoUSD = document.getElementById('MontoUSD');
+//             let MontoEUR = document.getElementById('MontoEUR');
+//             let CBU = document.getElementById('CBU');
+//             let Apertura = document.getElementById('Apertura');
+
+//             Banco.textContent = banco;
+
+
+
+
+
+//     }
+
+
+// })
